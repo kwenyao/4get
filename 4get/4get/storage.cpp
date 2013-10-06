b2c25e75b15ba0afe2420d63b69bd37e4f09a453
Wen Yao <kwenyao@users.noreply.github.com>
2013-10-06 17:13:30 +0800

    Added storage and task list source files

---
+++ 4get/4get/storage.cpp
@@ -0,0 +1,16 @@
+#include "storage.h"
+
+bool save(int listSize){
+	ofstream tempSaveFile("temp_save.4get");
+	tempSaveFile << listSize << endl;
+	for(int i=0; i<listSize; i++){
+		
+	}
+
+	tempSaveFile.close();
+	return false; //stub
+}
+
+bool load(){
+	
+}
