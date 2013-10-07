01ea1fc560d3d9b2a79f907e6a561e0a873657ac
patrick <patrick@localhost>
2013-10-08 01:08:47 +0800

    Executor in progress

---
+++ 4get/4get/logic_executor.cpp
@@ -0,0 +1,74 @@
+//#include "logic_executor.h"
+//
+//using namespace std;
+//
+//Executor::Executor(){}
+//
+//
+//bool Executor::receive(string usercommand, vector<string> parameter)
+//{
+//	Command commandType = determineCommandType(usercommand);
+//
+//	switch(commandType)
+//	{
+//	case ADD:
+//		return true;
+//	case DELETE:
+//		return true;
+//	default: return false;
+//	}
+//}
+//Enum::Command Executor::determineCommandType (string commandTypeString)
+//{
+//	if(isEqual(commandTypeString, COMMAND_ADD))
+//		return Command::ADD;
+//	else if(isEqual(commandTypeString, COMMAND_DELETE))
+//		return Command::DELETE;
+//	else
+//		return Command::INVALID;
+//}
+//bool Executor::adderFunction(vector<string>& vectorOfInputs)
+//{
+//	_task.setTaskDescription(vectorOfInputs[SLOT_DESCRIPTION]);
+//	_task.setTaskLocation(vectorOfInputs[SLOT_LOCATION]);
+//
+//}
+//bool Executor::isEqual(string str1, const string str2)
+//{
+//	if(str1 == str2)
+//		return true;
+//	return false;
+//}
+//string Executor::stringCollector(string task)
+//{
+//	return task;
+//}
+//void Executor::loadListOfTasks()
+//{
+//	tasks.loadListFromFile();	
+//}
+//int Executor::convertStringToInt(vector<string>& vectorOfInputs)
+//{
+//	string date = vectorOfInputs[SLOT_TIME];
+//	string day, month, year;
+//	int dayInt, monthInt, yearInt;
+//
+//
+//	day += date[0];
+//	day += date[1];
+//	month += date[3];
+//	year += date[5];
+//	year += date[6];
+//
+//
+//	cout << day<< endl;
+//	cout << month << endl;
+//	cout << year << endl;
+//
+//	stringstream sd(day);
+//	sd >> dayInt;
+//	stringstream sm(month);
+//	sm >> monthInt;
+//	stringstream sy(year);
+//	sy >> yearInt;
+//}
---
+++ 4get/4get/logic_executor.h
@@ -0,0 +1,48 @@
+/////////////////////////////////////
+// this function receives an array //
+//   with parameters form input    //
+//  and calls the approriate list  //
+/////////////////////////////////////
+
+#ifndef _LOGIC_EXECUTOR_H_
+#define _LOGIC_EXECUTOR_H_
+
+#include "logic_task.h"
+#include "logic_parser.h"
+#include "logic_task_list.h"
+#include <locale.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+using namespace Message;
+using namespace Enum;
+using namespace Constants;
+
+
+class Executor
+{
+	private:
+	Task _task;
+	TaskList tasks;
+	string task;
+	vector<string> vectorOfInputs(int SLOT_SIZE);
+
+public:
+	Executor();
+
+	string stringCollector(string task);
+	bool receive(string command, vector<string> parameters);
+	Command determineCommandType (string commandTypeString); 
+	void loadListOfTasks();
+	bool determineTaskType();
+	bool adderFunction(vector<string>& vectorOfInputs);
+
+
+	//helper functions
+	bool isEqual(string str1, const string str2);
+	string determineListType(tm);
+	tm convertStringToTm(vector<string>& vectorOfInputs);
+	int convertStringToInt(vector<string>& vectorOfInputs);
+};
+#endif
