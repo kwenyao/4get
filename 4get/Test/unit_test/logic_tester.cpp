aec1399175692d90c426f6e675be5cf7899040ff
Wen Yao <kwenyao@users.noreply.github.com>
2013-10-11 18:35:08 +0800

    [TaskList] moved function to retrieve current date and to obtain task ID to executor Also improved on the delete function such that only the index is required.

---
+++ 4get/Test/unit_test/logic_tester.cpp
@@ -0,0 +1,25 @@
+#include <gtest\gtest.h>
+#include "logic_tester.h"
+
+
+TEST_F(ParserTest, MethodReturnsFirstItem)
+{
+	vector<string> inputBits1(SLOT_SIZE);
+	vector<string> inputBits2(SLOT_SIZE);
+	vector<string> inputBits3(SLOT_SIZE);
+	vector<string> inputBits4(SLOT_SIZE);
+
+	TaskDeadline Dtask(id, description, location, reminderTime, priority, status, repeat, endTime);
+
+	parser.parseInput(input1,inputBits1);
+	parser.parseReset();
+	cout << Dtask.getTaskDescription() << endl;
+
+	parser.parseInput(input2, inputBits2);
+	parser.parseReset();
+	parser.parseInput(input3, inputBits3);
+	parser.parseReset();
+	parser.parseInput(input4, inputBits4);
+	parser.parseReset();
+
+}
