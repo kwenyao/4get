c42b06f4a15202fc9b85389e6ff71beb8488194d
Wen Yao <kwenyao@users.noreply.github.com>
2013-10-12 17:19:25 +0800

    [CommonMessage] Added enum for TimeType and constants for time conversion (tm -> int) in task

---
+++ 4get/4get/logic_task.h
@@ -0,0 +1,66 @@
+#ifndef _LOGIC_TASK_H_
+#define _LOGIC_TASK_H_
+#include <string>
+#include <time.h>
+#include <ctime>
+#include "common_message.h"
+
+// handled by John Kuan (A0097592M)
+
+using namespace std;
+using namespace Enum;
+using namespace Constants;
+
+class Task 
+{	
+protected:
+
+	//attributes
+	int taskId;
+	//TaskType taskType;
+	string taskDescription;
+	Priority taskPriority;
+	string taskLocation;
+	tm *taskReminderTime;
+	Status taskStatus;
+	RepeatType taskRepeat;
+	tm *taskNextOccurance;
+	tm *taskStart;
+	tm *taskEnd;
+	
+
+public:
+	Task();
+	virtual void setupTask(int id, string description, string location, tm *reminderTime, Priority priority, Status status);
+	//Task(string description, Priority priority, string location, Status status, Repeat_Type repeat, tm *reminder);
+
+	//getter functions
+	int getTaskId();
+	string getTaskDescription();
+	Priority getTaskPriority();
+	string getTaskLocation();
+	Status getTaskStatus();
+	tm* getTaskReminderTime();
+	virtual tm* getTaskStart();
+	virtual tm* getTaskEnd();
+	virtual RepeatType getTaskRepeat();
+
+	//setter functions
+	void setTaskId(int id);
+	void setTaskDescription(string description);
+	void setTaskPriority(Priority priority);
+	void setTaskLocation(string location);
+	void setTaskStatus(Status status);
+	void setTaskReminderTime(tm* remindTime);
+	virtual void setTaskStart(tm *startTask);
+	virtual void setTaskEnd(tm *endTask);
+	virtual void setTaskRepeat(RepeatType repeat); 
+	
+	//update function
+	void updateAllAttr();
+
+	void clearTimeAttr();
+	void clearAllAttr();
+
+};
+#endif
---
+++ 4get/4get/storage.cpp
@@ -0,0 +1,19 @@
+#include "storage.h"
+
+Storage::Storage(){}
+
+bool Storage::save(list<Task> list, ListType listType, int listSize){
+	ofstream tempSaveFile("temp_save.4get");
+	tempSaveFile << listSize << endl;
+	for(int i=0; i<listSize; i++){
+		
+	}
+	tempSaveFile.close();
+	return false; //stub
+}
+
+int Storage::load(list<Task> list,ListType listType ){
+	int listSize = 0;
+
+	return listSize;
+}
