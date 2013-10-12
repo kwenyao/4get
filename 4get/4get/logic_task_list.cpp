905e7b1fecae3e7a17868537e4e5877b89b5ef30
John Kuan <yukithesnowman@users.noreply.github.com>
2013-10-12 17:25:17 +0800

    Added a new function to Task class to return a string of integers of a specified tm

---
+++ 4get/4get/logic_task_list.cpp
@@ -0,0 +1,91 @@
+#include "logic_task_list.h"
+
+TaskList::TaskList(){
+}
+
+bool TaskList::loadFromFile(){
+	storage.load(toDoList, listToDo);
+	storage.load(completedList, listCompleted);
+	storage.load(overdueList, listOverdue);
+	return 0; //stub
+}
+
+bool TaskList::saveToFile(){
+	storage.save(toDoList, listToDo, toDoListSize);
+	storage.save(completedList, listCompleted, completedListSize);
+	storage.save(overdueList, listOverdue, overdueListSize);
+	return 0; //stub
+}
+
+bool TaskList::addToList(Task task, ListType listToAdd){
+	switch (listToAdd){
+	case listToDo:
+		return addToDoList(task);
+	case listCompleted:
+		return addCompletedList(task);
+	case listOverdue:
+		return addOverdueList(task);
+	}
+	return false; 
+}
+
+bool sort(list<Task> listToSort){
+	
+	return false; //stub
+}
+
+bool TaskList::addToDoList(Task task){
+	
+	toDoList.push_back(task);
+	sort(toDoList);
+	return 0; //stub
+}
+
+bool TaskList::addCompletedList(Task task){
+	completedList.push_back(task);
+	sort(completedList);
+	return 0; //stub
+}
+
+bool TaskList::addOverdueList(Task task){
+	overdueList.push_back(task);
+	sort(overdueList);
+	return 0; //stub
+}
+
+bool TaskList::deleteFromList(int taskToDelete){
+	list<Task>::iterator iterator;
+	for(int i=0; i<taskToDelete; i++)
+		++iterator;
+	switch (currentDisplayed){
+	case listToDo:
+		toDoList.erase(iterator);
+		break;
+	case listCompleted:
+		completedList.erase(iterator);
+		break;
+	case listOverdue:
+		overdueList.erase(iterator);
+		break;
+	default:
+		return false;
+	}
+	return true; //stub
+}
+
+list<Task> TaskList::obtainList(ListType listToReturn){
+	switch (listToReturn){
+	case listToDo:
+		listToDisplay = toDoList;
+		break;
+	case listCompleted:
+		listToDisplay = completedList;
+		break;
+	case listOverdue:
+		listToDisplay = overdueList;
+		break;
+	}
+	currentDisplayed = listToReturn;
+	return listToDisplay;
+}
+
