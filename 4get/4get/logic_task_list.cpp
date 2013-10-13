3a2c88b39ddd6c339bb929bfc8569cad95e3ad83
Ho Jia Yi <jiayi_h@yahoo.com.sg>
2013-10-13 20:23:06 +0800

    [GUI] - change completed list view

---
+++ 4get/4get/logic_task_list.cpp
@@ -0,0 +1,79 @@
+#include "logic_task_list.h"
+
+const int TaskList::DELETE_INDEX_CORRECTION = -1;
+
+TaskList::TaskList(){
+}
+
+bool TaskList::loadFromFile(){
+	_storage.load(_toDoList, listToDo);
+	_storage.load(_completedList, listCompleted);
+	_storage.load(_overdueList, listOverdue);
+	return 0; //stub
+}
+
+bool TaskList::saveToFile(){
+	_storage.save(_toDoList, listToDo, _toDoListSize);
+	_storage.save(_completedList, listCompleted, _completedListSize);
+	_storage.save(_overdueList, listOverdue, _overdueListSize);
+	return 0; //stub
+}
+
+bool TaskList::addToList(Task task, ListType listType){
+	list<Task>* listToAdd;
+	list<Task>::iterator iterator;
+	listToAdd = determineList(listType);
+	iterator = getIterator(*listToAdd, task);
+	listToAdd->insert(iterator,task);
+	return true;
+}
+
+bool TaskList::deleteFromList(int taskToDelete){
+	list<Task>* listToDeleteFrom;
+	list<Task>::iterator iterator;
+	taskToDelete = taskToDelete + DELETE_INDEX_CORRECTION;
+	listToDeleteFrom = determineList(_currentDisplayed);
+	iterator = listToDeleteFrom->begin();
+	for(int i=0; i<taskToDelete; i++)
+		++iterator;
+	listToDeleteFrom->erase(iterator);
+	return true; //stub
+}
+
+list<Task> TaskList::obtainList(ListType listToReturn){
+	list<Task>* listPtr;
+	listPtr = determineList(listToReturn);
+	_listToDisplay = *listPtr;
+	_currentDisplayed = listToReturn;
+	return _listToDisplay;
+}
+
+list<Task>::iterator TaskList::getIterator(list<Task>& insertionList, Task taskToAdd){
+	long long tempTime;
+	bool isEmpty = insertionList.empty();
+	long long taskEndTime = taskToAdd.getTimeInt(timeEnd);
+	list<Task>::iterator iterator = insertionList.begin();
+	if(isEmpty){
+		return iterator;
+	}
+	int listSize = insertionList.size();
+	for(int i=0; i<listSize; i++){
+		tempTime = iterator->getTimeInt(timeEnd);
+		if(tempTime > taskEndTime){
+			return iterator;
+		}
+		++iterator;
+	}
+	return iterator;
+}
+
+list<Task>* TaskList::determineList(ListType listType){
+	switch (listType){
+	case listToDo:
+		return &_toDoList;
+	case listCompleted:
+		return &_completedList;
+	case listOverdue:
+		return &_overdueList;
+	}
+}
