8711f4b2d2dbbcfc71d266202972eb29f2adc20f
Ho Jia Yi <jiayi_h@yahoo.com.sg>
2013-10-24 11:07:00 +0800

    [GUI] - added more test to navigate tabs

---
+++ 4get/4get/storage.h
@@ -0,0 +1,110 @@
+#ifndef _STORAGE_H_
+#define _STORAGE_H_
+
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "logic_task.h"
+#include "logic_task_floating.h"
+#include "logic_task_deadline.h"
+#include "logic_task_timed.h"
+#include "Log.h"
+
+using namespace std;
+
+class Storage{
+private:
+	ofstream _fileWrite;
+	ifstream _fileRead;
+	list<Task> _taskList;
+
+	//constants
+	static const string FILENAME_TODO;
+	static const string FILENAME_COMPLETED;
+	static const string FILENAME_OVERDUE;
+
+	//markers
+	static const string MARKER_TASK_BEGIN;
+	static const string MARKER_TASK_TYPE;
+	static const string MARKER_ID;
+	static const string MARKER_DESCRIPTION;
+	static const string MARKER_LOCATION;
+	static const string MARKER_START_TIME;
+	static const string MARKER_END_TIME;
+	static const string MARKER_REPEAT;
+	static const string MARKER_NEXT_OCCURANCE;
+	static const string MARKER_PRIORITY;
+	static const string MARKER_REMINDER_TIME;
+	static const string MARKER_TASK_END;
+	static const string EMPTY_LINE;
+
+	static const string TASK_TYPE_FLOATING;
+	static const string TASK_TYPE_DEADLINE;
+	static const string TASK_TYPE_TIMED;
+
+	static const string REPEAT_TYPE_DAILY;
+	static const string REPEAT_TYPE_WEEKLY;
+	static const string REPEAT_TYPE_FORTNIGHTLY;
+	static const string REPEAT_TYPE_MONTHLY;
+	static const string REPEAT_TYPE_ANNUALLY;
+	static const string REPEAT_TYPE_NONE;
+
+	static const string PRIORITY_HIGH;
+	static const string PRIORITY_NORMAL;
+
+	void writeFileSetup(string fileName, ios_base::openmode mode);
+	string determineFile(ListType listType);
+
+	bool saveList(const list<Task*>& listToSave);
+	void saveTaskToFile(const Task& taskToSave);
+	void closeFileWrite();
+	void writeTaskAttributes(const Task& task);
+	void writeLine(string line);
+
+	void writeTaskType(const Task& task);
+	void writeID(const Task& task);
+	void writeDescription(const Task& task);
+	void writeLocation(const Task& task);
+	void writeStartTime(const Task& task);
+	void writeEndTime(const Task& task);
+	void writeRepeat(const Task& task);
+	void writeNextOccurance(const Task& task);
+	void writePriority(const Task& task);
+	void writeReminderTime(const Task& task);
+
+	//Functions to convert to string
+	//string convertToStr(long long number);
+	string convertToStr(tm* time);
+	string convertToStr(time_t time);
+	string convertToStr(TaskType taskType);
+	string convertToStr(RepeatType repeatType);
+	string convertToStr(Priority priority);
+	
+
+	//load functions
+	void readFileSetup(string fileName);
+	void closeFileRead();
+	void loadIntoList(list<Task*>& listToLoad);
+	bool isLoadIncomplete();
+	bool foundNextTask();
+	string getNextLine();
+	Task* getNewTask();
+	Task* constructTask(TaskType taskType, string IDString);
+	void insertTaskIntoList (list<Task*>& listToInsert, Task* task);
+	list<Task*>::iterator getIterator(list<Task*>& insertionList, Task* task);
+
+	TaskType convertToTaskType(string str);
+	int convertToInt(string str);
+	time_t convertToTime(string str);
+	RepeatType convertToRepeatType(string str);
+	Priority convertToPriority(string str);
+
+public:
+	//constructor
+	Storage();
+
+	bool save(const list<Task*>& list,ListType listType);
+	bool load(list<Task*>& listToLoadTo, ListType listType);
+};
+#endif
---
+++ 4get/4get/ui_display.h
@@ -0,0 +1,214 @@
+#pragma once
+
+#include "ui_convert.h"
+#include "logic_executor.h"
+#include "logic_task.h"
+#include "common_message.h"
+#include <string>
+#include <list>
+#include <time.h>
+#include <Windows.h>
+
+namespace UIDisplay {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for ui_display
+	/// </summary>
+	public ref class ui_display : public System::Windows::Forms::Form
+	{
+	public:
+		ui_display(void);
+	protected:
+		~ui_display();
+#pragma endregion
+
+#pragma region initialise
+	private: UiConvert* converter;
+	private: Executor* execute;
+	private: list<Task*>* listOfTasks;
+
+	private: bool loaded;
+	private: string *commandKeyword;
+
+	private: System::Windows::Forms::TabControl^  tabContainer;
+	private: System::Windows::Forms::TabPage^  tabTodo;
+	private: System::Windows::Forms::TabPage^  tabCompleted;
+	private: System::Windows::Forms::TabPage^  tabOverdue;
+	private: System::Windows::Forms::TextBox^  textboxInput;
+	private: System::Windows::Forms::FlowLayoutPanel^  inputContainer;
+	private: ListType activeListType;
+
+
+	private: System::Windows::Forms::FlowLayoutPanel^  messageContainer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private: System::Windows::Forms::RichTextBox^  messageBox;
+
+
+
+	private: System::Windows::Forms::GroupBox^  todayContainer;
+	private: System::Windows::Forms::CheckedListBox^  checkedTaskList;
+	private: System::Windows::Forms::DateTimePicker^  chooseDate;
+
+
+
+
+
+
+
+
+	private: System::Windows::Forms::ListView^  todoListView;
+	private: System::Windows::Forms::ColumnHeader^  tIndex;
+
+	private: System::Windows::Forms::ColumnHeader^  tDescription;
+	private: System::Windows::Forms::ColumnHeader^  tVenue;
+	private: System::Windows::Forms::ColumnHeader^  tTime;
+	private: System::Windows::Forms::ColumnHeader^  tDueDate;
+	private: System::Windows::Forms::ColumnHeader^  tPriority;
+	private: System::Windows::Forms::ListView^  completedListView;
+	private: System::Windows::Forms::ColumnHeader^  cIndex;
+	private: System::Windows::Forms::ColumnHeader^  cDescription;
+	private: System::Windows::Forms::ColumnHeader^  cVenue;
+	private: System::Windows::Forms::ColumnHeader^  cTime;
+	private: System::Windows::Forms::ColumnHeader^  cDueDate;
+	private: System::Windows::Forms::ColumnHeader^  cPriority;
+
+
+
+
+
+
+	private: System::Windows::Forms::ListView^  overdueListView;
+	private: System::Windows::Forms::ColumnHeader^  oIndex;
+	private: System::Windows::Forms::ColumnHeader^  oDescription;
+	private: System::Windows::Forms::ColumnHeader^  oVenue;
+	private: System::Windows::Forms::ColumnHeader^  oTime;
+	private: System::Windows::Forms::ColumnHeader^  oDueDate;
+	private: System::Windows::Forms::ColumnHeader^  oPriority;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void);
+#pragma endregion
+
+	private: void printList();
+	private: void printToDoList();
+	private: void printCompletedList();
+	private: void printOverdueList();
+	private: void passUserInput();
+	private: void printError(string error);
+	public: System::Void textboxInput_KeyDown(System::Object^  sender, System::Windows::Forms::KeyEventArgs^  e);
+	private: System::Void textboxInput_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e);
+	private: System::Void textboxInput_KeyPress(System::Object^  sender, System::Windows::Forms::KeyPressEventArgs^  e);
+	private: System::Void checkInput();
+	private: System::Void printAddMessage();
+	private: System::Void printDelMessage();
+	private: System::Void printModMessage();
+	private: System::Void printMarMessage();
+	private: System::Void printHelpMessage();
+	private: bool SetFocus(Control ^ control);
+	private: System::Void tabContainer_Selected(System::Object^  sender, System::Windows::Forms::TabControlEventArgs^  e);
+	private: System::Void ui_display_KeyDown(System::Object^  sender, System::Windows::Forms::KeyEventArgs^  e);
+	private: System::Void ui_display_KeyPress(System::Object^  sender, System::Windows::Forms::KeyPressEventArgs^  e);
+};
+}
