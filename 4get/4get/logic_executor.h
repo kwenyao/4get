8f0a0d2a4be33bf7fac241185db3eb958717e9c9
Ho Jia Yi <jiayi_h@yahoo.com.sg>
2013-11-07 07:26:46 +0800

    [GUI] - fixed error with messages, added magic strings

---
+++ 4get/4get/logic_executor.h
@@ -0,0 +1,92 @@
+/////////////////////////////////////
+// this function receives an array //
+//   with parameters form input    //
+//  and calls the approriate list  //
+/////////////////////////////////////
+#ifndef _LOGIC_EXECUTOR_H_
+#define _LOGIC_EXECUTOR_H_
+
+#include <assert.h>
+#include "common_message.h"
+#include "log.h"
+#include "logic_converter.h"
+#include "logic_parser.h"
+#include "logic_task.h"
+#include "logic_task_list.h"
+#include "logic_task_floating.h"
+#include "logic_task_deadline.h"
+#include "logic_task_timed.h"
+#include <stack>
+#include <locale.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+using namespace Message;
+using namespace Enum;
+using namespace Constants;
+
+
+class Executor
+{
+private:
+	string task;
+	Task *taskGlobal;
+	TaskList taskList;
+	Parser parser;
+	Converter convert;
+	stack<Task> undoTaskStack;
+	stack<Command> undoCommandStack;
+	stack<Task> redoTaskStack;
+	stack<Command> redoCommandStack;
+	stack<int> undoDeleteNumberStack;
+	stack<int> redoDeleteNumberStack;
+	stack<ListType> undoListTypeStack;
+	stack<ListType> redoListTypeStack;
+	ListType listType;
+
+	Task* createTaskPtr(Task taskToCreate);
+
+public:
+	//constructor
+	Executor();
+
+	//UI call these functions
+	bool stringCollector(string task);
+	void refreshAll();
+	bool receive(string command, vector<string> vectorOfInputs);
+	Command determineCommandType (string commandTypeString); 
+	list<Task*> getUpdatedList(ListType listType);
+
+	//Functions to be executed
+	bool adderFunction(vector<string> vectorOfInputs);
+	bool modifyFunction(vector<string> vectorOfInputs);
+	bool deleteFunction(vector<string> vectorOfInputs);
+	bool markFunction(vector<string> vectorOfInputs);
+	bool undoFunction();
+	bool redoFunction();
+	bool searchFunction(vector<string> vectorOfInputs);
+	bool showAllFunction();
+	
+	//helper functions
+	bool isEqual(string str1, const string str2);
+	long long retrieveTaskID();
+	bool helperDeleteFunction(int deleteStartNumber);
+	bool storeIntoUndoTaskStack(Task taskTemp);
+	bool storeIntoUndoCommandStack(Command command);
+	bool storeIntoRedoTaskStack(Task taskTemp);
+	bool storeIntoRedoCommandStack(Command command);
+	bool setListType(ListType listType);
+	bool setParameters(string &description, 
+						string &location, 
+						Priority &priority,
+						RepeatType &repeat,
+						time_t &startTime, 
+						time_t &endTime, 
+						time_t &reminderTime, 
+						TaskType &typeOfTask,
+						vector<string> &vectorOfInputs);
+	//assertions
+	void assertNotEmptyTask();
+};
+#endif
