d2491c277c8e24ae83adf7f513aa348bc75cd7e7
John Kuan <yukithesnowman@users.noreply.github.com>
2013-11-08 08:47:50 +0800

    merged

---
+++ 4get/4get/logic_parser.h
@@ -0,0 +1,179 @@
+#ifndef _LOGIC_PARSER_H_
+#define _LOGIC_PARSER_H_
+#define PROGRAM_NAME "4get"
+#include <ctype.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <assert.h>
+#include "common_message.h"
+#include "Log.h"
+
+// handled by John Kuan (A0097592M)
+
+using namespace std;
+using namespace Enum;
+using namespace Commands;
+using namespace Message;
+using namespace Constants;
+class Parser
+{
+	vector< vector<string> > dictionaryCommands;
+	vector<string> dictionaryAddCommands;
+	vector<string> dictionaryDeleteCommands;
+	vector<string> dictionaryModifyCommands;
+	vector<string> dictionaryMarkCommands;
+	vector<string> dictionaryUndoCommands;
+	vector<string> dictionaryRedoCommands;
+	vector<string> dictionaryShowCommands;
+	vector<string> dictionaryShowAllCommands;
+	vector<string> dictionaryDates;
+	vector<string> dictionaryMonth;
+	vector<string> dictionaryTime;
+	vector<string> dictionaryMarker;
+
+private:
+
+	static const char MARKER;
+	static const std::size_t MARKER_LENGTH;
+	static const char MARKER_COMMA;
+	static const std::size_t MARKER_COMMA_LENGTH;
+	static const char MARKER_ENCLOSE;
+	static const std::size_t MARKER_ENCLOSE_LENGTH;
+	static const string MARKER_SPACE;
+	static const std::size_t MARKER_SPACE_LENGTH;
+	static const string MARKER_AT;
+	static const std::size_t MARKER_AT_LENGTH;
+	static const string MARKER_NEAR;
+	static const std::size_t MARKER_NEAR_LENGTH;
+	static const string MARKER_BY;
+	static const std::size_t MARKER_BY_LENGTH;
+	static const string MARKER_DUE;
+	static const std::size_t MARKER_DUE_LENGTH;
+	static const string MARKER_FROM;
+	static const std::size_t MARKER_FROM_LENGTH;
+	static const string MARKER_TO;
+	static const std::size_t MARKER_TO_LENGTH;
+	static const string MARKER_COMMA_TO;
+	static const std::size_t MARKER_COMMA_TO_LENGTH;
+	static const string MARKER_REMIND;
+	static const std::size_t MARKER_REMIND_LENGTH;
+	static const string MARKER_ON;
+	static const std::size_t MARKER_ON_LENGTH;
+	static const string MARKER_REPEAT;
+	static const std::size_t MARKER_REPEAT_LENGTH;
+	static const string MARKER_PRIORITY;
+	static const std::size_t MARKER_PRIORITY_LENGTH;
+	static const string MARKER_PRIORITY_HIGH;
+	static const std::size_t MARKER_PRIORITY_HIGH_LENGTH;
+	static const string MARKER_PRIORITY_NORMAL;
+	static const std::size_t MARKER_PRIORITY_NORMAL_LENGTH;
+	static const string MARKER_DONE;
+	static const std::size_t MARKER_DONE_LENGTH;
+	static const string MARKER_COMPLETED;
+	static const std::size_t MARKER_COMPLETED_LENGTH;
+	static const string MARKER_UNDONE;
+	static const std::size_t MARKER_UNDONE_LENGTH;
+	static const string MARKER_INCOMPLETE;
+	static const std::size_t MARKER_INCOMPLETE_LENGTH;
+
+	static const int DICTIONARY_ADD;
+	static const int DICTIONARY_DELETE;
+	static const int DICTIONARY_MODIFY;
+	static const int DICTIONARY_MARK;
+	static const int DICTIONARY_UNDO;
+	static const int DICTIONARY_REDO;
+	static const int DICTIONARY_SHOW;
+	static const int DICTIONARY_SHOWALL;
+
+	//istringstream iss;
+	string _textInput; // Main input
+	string textInput; // lower case input
+	string textCommand;
+	string textDescription;
+	string textVenue;
+	string textStart;
+	string textStartDate;
+	string textStartTime;
+	string textEnd;
+	string textEndDate;
+	string textEndTime;
+	string textRepeat;
+	string textPriority;
+	//string textStatus;
+	string textRemind;
+	string textRemindDate;
+	string textRemindTime;
+	string textSlotStartNumber;
+	string textSlotEndNumber;
+
+public:
+	Parser();
+
+	// callable by executor
+
+	void parseInput(string input, vector<string>& inputBits);
+	// function for testing validity of command
+	string getCommand();
+	bool parseTimeAndDate(string& str, string& strDate, string& strTime);
+
+private:	
+	void parseReset();
+	void processCommand(string commandString, vector<string>& inputBits);
+	void populateContainer(vector<string>& inputBits);
+
+	//separate functions
+	bool separateInput(Command userCommand, vector<string>& inputBits);
+	bool separateFunctionAdd(vector<string>& inputBits);
+	bool separateFunctionDelete(vector<string>& inputBits);
+	//bool separateFunctionDeleteAll(vector<string>& inputBits);
+	bool separateFunctionMark(vector<string>& inputBits);
+	//bool separateFunctionMarkDone(vector<string>& inputBits);
+	bool separateFunctionModify(vector<string>& inputBits);
+	bool separateFunctionUndo(vector<string>& inputBits);
+	bool separateFunctionShow(vector<string>& inputBits); // Show, Display, GoTo. This show user task in a timespan.
+	bool separateFunctionRedo(vector<string>& inputBits);
+	bool separateFunctionShowAll(vector<string>& inputBits);
+
+	bool determineVenue();
+	bool determineDateAndTime();
+	bool determineReminder();
+	bool determinePriority();
+	bool determineRepeat();
+	bool determineSlot();
+	//bool determineTaskList();
+	bool determineStatus();
+	std::size_t determindExtractLength(std::size_t found, std::size_t foundComma, string markerConstant, std::size_t& extractStart);
+
+	void shortenInput(std::size_t partitionStart, std::size_t partitionEnd);
+	void removeAllBorderSpace();
+	void removeBorderSpaces(string& str);
+	void parseAllTimeAndDate();
+
+	//int separateInput(int state);
+	void toLowerCase(string &string);
+	string getFirstWord(string input);
+	void removeFirstWord(string &input);
+	bool isParseInt(string input, int &value);
+	bool isInt(string input);
+
+	//parser dictionary setup
+	void loadDictionary();
+	void loadDatesDictionary();
+	void loadMonthDictionary();
+	void loadTimeDictionary();
+	void loadCommandDictionary();
+	void loadMarkerDictionary();
+
+	//parser scan dictionaries
+	bool scanDictionary(string);
+	bool scanTimerDictionary(string);
+	bool scanTimeDictionary(string);
+	bool scanDatesDictionary(string);
+	bool scanMonthDictionary(string);
+	string scanCommandDictionary(string);
+	bool scanMarkerDictionary(string);
+};
+#endif
---
+++ 4get/4get/logic_task_deadline.cpp
@@ -0,0 +1,98 @@
+#include "logic_task_deadline.h"
+
+TaskDeadline::TaskDeadline(){
+	taskType = deadline;
+}
+TaskDeadline::TaskDeadline(long long id)
+{
+	clearAllAttr();
+	taskType = deadline;
+	taskId = id;
+}
+
+TaskDeadline::TaskDeadline(long long id, string description, string location, time_t reminder, Priority priority, RepeatType repeat, time_t endTime) : Task()
+{
+	TaskType type = deadline;
+	setupTask(id, type, description, location, reminder, priority, repeat, endTime);
+}
+
+
+void TaskDeadline::setupTask(long long id, TaskType type, string description, string location, time_t reminder, Priority priority, RepeatType repeat, time_t endTime)
+{
+	taskId = id;
+	taskType = type;
+	taskDescription = description;
+	taskPriority = priority;
+	taskLocation = location;
+	taskReminder = reminder;
+	taskRepeat = repeat;
+	taskEnd = endTime;
+}
+void TaskDeadline::setTaskEnd(time_t endTime) {	taskEnd = endTime; }
+
+void TaskDeadline::setNextOccurance(){
+	struct tm timeTM = convertToTm(taskEnd);
+	switch (taskRepeat){
+	case Enum::daily:
+		timeTM.tm_mday = timeTM.tm_mday + 1;
+		break;
+	case Enum::weekly:
+		timeTM.tm_mday = timeTM.tm_mday + 7;
+		break;
+	case Enum::fortnightly:
+		timeTM.tm_mday = timeTM.tm_mday + 14;
+		break;
+	case Enum::monthly:
+		timeTM.tm_mon = timeTM.tm_mon + 1;
+		break;
+	case Enum::annually:
+		timeTM.tm_year = timeTM.tm_year + 1;
+		break;
+	default:
+		return;
+	}
+	taskEnd = mktime(&timeTM);
+}
+
+time_t TaskDeadline::getTaskEnd() {	return taskEnd; }
+
+long long TaskDeadline::getTimeLong(TimeType type){
+	time_t temp = 0;
+	switch (type){
+	case Enum::timeEnd:
+		temp = taskEnd;
+		break;
+	case Enum::timeNext:
+		temp = taskNextOccurance;
+		break;
+	case Enum::timeReminder:
+		temp = taskReminder;
+		break;
+	default:
+		return 0;
+	}
+	return convertToLong(temp);
+}
+
+long long TaskDeadline::convertToLong(time_t rawTime){
+	long long yearMonthDayHourMin;
+	tm* time = {0};
+	time = localtime(&rawTime);
+	long long min = time->tm_min;
+	long long hour = time->tm_hour;
+	long long day = time->tm_mday;
+	long long month = time->tm_mon;
+	long long year = time->tm_year + YEAR_CORRECTION;
+	//hour = hour*CONVERT_MULTIPLIER_HOUR;
+	day = day*CONVERT_MULTIPLIER_DAY;
+	month = month*CONVERT_MULTIPLIER_MONTH;
+	year = year*CONVERT_MULTIPLIER_YEAR;
+	yearMonthDayHourMin = (year + month + day);
+	return yearMonthDayHourMin;
+}
+
+tm TaskDeadline::convertToTm(time_t rawTime){
+	struct tm returnTime;
+	localtime_s(&returnTime,&rawTime);
+	return returnTime;
+}
