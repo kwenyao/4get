bd861ecc175295f13dd3b5d6aaea0701bf2b30cb
Wen Yao <kwenyao@users.noreply.github.com>
2013-11-10 17:30:25 +0800

    [Converter] Removed time dictionary (unused) [Common message] removed some unused constants/made some arrangements

---
+++ 4get/4get/storage.h
@@ -0,0 +1,115 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  storage.h
+ *
+ *         Author:  KOH WEN YAO (A0097210M), kwenyao@nus.edu.sg
+ *   Organization:  NUS, School of Computing
+ *
+ * =====================================================================================
+ */
+
+#ifndef _STORAGE_H_
+#define _STORAGE_H_
+
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "logic_task.h"
+#include "logic_task_floating.h"
+#include "logic_task_deadline.h"
+#include "logic_task_timed.h"
+#include "Log.h"
+
+using namespace std;
+
+class Storage{
+private:
+	ofstream _fileWrite;
+	ifstream _fileRead;
+	list<Task> _taskList;
+
+	//constants
+	static const string FILENAME_TODO;
+	static const string FILENAME_COMPLETED;
+	static const string FILENAME_OVERDUE;
+
+	//markers
+	static const string MARKER_TASK_BEGIN;
+	static const string MARKER_TASK_TYPE;
+	static const string MARKER_ID;
+	static const string MARKER_DESCRIPTION;
+	static const string MARKER_LOCATION;
+	static const string MARKER_START_TIME;
+	static const string MARKER_END_TIME;
+	static const string MARKER_REPEAT;
+	static const string MARKER_PRIORITY;
+	static const string MARKER_TASK_END;
+	static const string EMPTY_LINE;
+
+	static const string TASK_TYPE_FLOATING;
+	static const string TASK_TYPE_DEADLINE;
+	static const string TASK_TYPE_TIMED;
+	static const string REPEAT_TYPE_DAILY;
+	static const string REPEAT_TYPE_WEEKLY;
+	static const string REPEAT_TYPE_FORTNIGHTLY;
+	static const string REPEAT_TYPE_MONTHLY;
+	static const string REPEAT_TYPE_ANNUALLY;
+	static const string REPEAT_TYPE_NONE;
+	static const string PRIORITY_HIGH;
+	static const string PRIORITY_NORMAL;
+
+	string determineFile(ListType listType);
+
+	//save functions
+	void writeFileSetup(string fileName, ios_base::openmode mode);
+	void closeFileWrite();
+	void saveList(const list<Task*>& listToSave);
+	void saveTaskToFile(const Task& taskToSave);
+	
+	//functions to write task attributes
+	void writeTaskAttributes(const Task& task);
+	void writeLine(string line);
+	void writeTaskType(const Task& task);
+	void writeID(const Task& task);
+	void writeDescription(const Task& task);
+	void writeLocation(const Task& task);
+	void writeStartTime(const Task& task);
+	void writeEndTime(const Task& task);
+	void writeRepeat(const Task& task);
+	void writePriority(const Task& task);
+
+	//load functions
+	void   readFileSetup(string fileName);
+	void   closeFileRead();
+	void   loadIntoList(list<Task*>& listToLoad);
+	void   insertTaskIntoList (list<Task*>& listToInsert, Task* task);
+	bool   isLoadIncomplete();
+	bool   foundNextTask();
+	string getNextLine();
+	Task*  getNewTask();
+	Task*  constructTask(TaskType taskType, string IDString);
+
+	//Functions to convert to string
+	string convertToStr(tm* time);
+	string convertToStr(time_t time);
+	string convertToStr(TaskType taskType);
+	string convertToStr(RepeatType repeatType);
+	string convertToStr(Priority priority);
+
+	//functions to convert from string
+	TaskType   convertToTaskType(string str);
+	int        convertToInt(string str);
+	time_t     convertToTime(string str);
+	RepeatType convertToRepeatType(string str);
+	Priority   convertToPriority(string str);
+
+public:
+	//constructor
+	Storage();
+	void clearFile(ListType);
+	void save(const list<Task*>& list,ListType listType);
+	void load(list<Task*>& listToLoadTo, ListType listType);
+};
+#endif
---
+++ 4get/4get/ui_convert.h
@@ -0,0 +1,48 @@
+#pragma once
+#ifndef UI_CONVERT_H
+#define UI_CONVERT_H
+
+#include <list>
+#include <string>
+#include <time.h>
+#include <sstream>
+#include "logic_task.h"
+#include "common_message.h"
+#include <vcclr.h>
+#include "msclr\marshal_cppstd.h"
+
+#define BUFFER_SIZE 255
+namespace UIConvert {
+	using namespace std;
+	using namespace System;
+	using namespace msclr::interop;
+
+	class UiConvert
+	{
+	public:
+		UiConvert();
+
+		static char systemMessage[BUFFER_SIZE];
+		static const bool isPrintSystemMessage = false;
+
+	public:
+		void stringStdToSysConversion(System::String^& result, string& source);
+		string timetToStdString(time_t time);
+		bool stringSysToStdConversion(System::String^ source, string& result);
+		string enumPriorityToStdString(Priority priority);
+		int stringSysToIntConversion(System::String^ source);
+		void intToSysString(System::String^& result, int source);
+		void printItem(System::Windows::Forms::ListViewItem^ item, std::list<Task*> *list, int taskIndex);
+
+	private:
+		static const int ITEM_INDEX_SLOT;
+		static const int TIME_BUFFER_SIZE;
+		static const int INITIALISE_INT_ZERO;
+		static const string INITIALISE_EMPTY_STD_STRING;
+		static const string HIGH_PRIORITY_TEXT;
+		static const string NORMAL_PRIORITY_TEXT;
+		static const time_t EMPTY_TIME;
+
+	};
+}
+#endif
